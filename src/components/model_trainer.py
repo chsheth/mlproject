8dc4bb4242d002ec73d6e61e14985efaeedca1d8
Chintan <chsheth@gmail.com>
2023-04-18 13:57:22 -0400

    hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -32,6 +32,11 @@ class ModelTrainer():
         self.model_trainer_config = ModelTrainerConfig()
 
     def initiate_model_trainer(self, train_array, test_array,):
+        '''
+        this function provides information for evaluate_model function in utils;
+        it trains on 
+        '''
+    
         try:
             logging.info("Split train and test file")
             X_train, y_train, X_test, y_test =(
@@ -51,29 +56,73 @@ class ModelTrainer():
                     "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
+            params = {
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
+            # function call for training and hyperparameter tuning
             model_report:dict = evaluate_model(X_train=X_train, y_train=y_train,
                                             X_test=X_test, y_test=y_test,
-                                            models=models)
+                                            models=models, params=params)
             
+            # selecting best score by picking the max test score on the dictionary output
+            # from evaluate_model
             best_model_score = max(sorted(model_report.values()))
 
+            # use the index of score selected as the best_score to get the corresponding
+            # model_name from model.keys()
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
 
                 ]
-            
+            # selecting the name from models dictionary
             best_model = models[best_model_name]
 
             if best_model_score < 0.6:
                 raise CustomException("Model not good enough")
-            logging.info(f'Best model found on train and test dataset')
+            logging.info(f'Best model {best_model_name} found on train and test dataset')
 
+            # saving the model pkl in the artifact folder
             save_object(
                 file_path = self.model_trainer_config.trained_model_file_path,             
                 obj = best_model
 
             )
-
+            # making prediction on the test data
             predicted = best_model.predict(X_test)
 
             r2_square = r2_score(y_test, predicted)
--- src/utils.py
+++ src/utils.py
@@ -4,33 +4,63 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
 def save_object(file_path, obj):
+    '''
+    - this function takes in file_path and object as arguments and saves the object
+    in that location;
+    - mainly for collecting model and data artifacts and saving them
+    in the artifact/
+    '''
     try:
         dir_path = os.path.dirname(file_path)
+        # check if folder(s) exists, else create
         os.makedirs(dir_path, exist_ok=True)
 
+        # in this folder location, create a dill object for what is passed
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
+    '''
+    - this function builds all the specified models using cross validation from the models dictionary;
+    - it also does hyperparameter tuning and then picks the best model with the best parameters;
+    - hyperparameter dictionary is provided
+    - it returns the model name and test evaluation metric of the selected model -- all compiled in a dictionary
+    '''
     try:
         report = {}
 
         for i in range(len(list(models))):
+            # select model from list of models
+            # select the corresponding dictionary of hyperparameters to try        
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
 
+            #gridsearch is on cross validated sample with 3 folds - on train data
+            gs = GridSearchCV(model,param,cv=3)
+            gs.fit(X_train,y_train)
+
+            #selecting best hyperparameters
+            model.set_params(**gs.best_params_)
+            
+            #training on the full set with the best parameters
             model.fit(X_train, y_train)
+
+            #make predictions
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
+
+            #prepare a dictionary of model name as key and test R2 as value
 
             report[list(models.keys())[i]] = test_model_score
 
